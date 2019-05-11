532bedc0ce93814d684e1137e02709e96baa367a
Jan Ficko <jan.ficko@gmail.com>
2019-05-11 17:09:53 +0200

    Created I Am Rich project

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Jan Ficko on 11/05/2019.
+//  Copyright © 2019 Jan Ficko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

6394c027e343ef818dcefad4322b87b3ca57f147
Jan Ficko <jan.ficko@gmail.com>
2019-05-20 12:21:58 +0200

    Added playground file

---
+++ Variables, Constants and Data Types.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var myAge: Int = 25
+myAge = 26
+
+let myName: String = "Jan"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = "\(myName) Ficko"
+
+let myDetails = "\(myFullName), \(myAge)"
+
+let booleans: Bool = true
+
+let floatingPointNumber: Float = 1.3
+
+let double: Double = 3.14159265359
+
+let stringArray: [String] = ["one", "two"]

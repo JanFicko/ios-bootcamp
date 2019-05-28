a4de2a646838d6e3aef76f45283adb49c2f1ce59
Jan Ficko <jan.ficko@gmail.com>
2019-05-28 10:25:02 +0200

    Added Calculator project to repository, added view attributes

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jan Ficko on 28/05/2019.
+//  Copyright © 2019 Jan Ficko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

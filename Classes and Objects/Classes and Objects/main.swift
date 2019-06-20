21fa3e5a2743e8c580f5686ccf0ca6dce06d8b32
Jan Ficko <jan.ficko@gmail.com>
2019-06-20 08:37:35 +0200

    Created weather app with network calls

---
+++ Classes and Objects/Classes and Objects/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Classes and Objects
+//
+//  Created by Jan Ficko on 28/05/2019.
+//  Copyright © 2019 Jan Ficko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

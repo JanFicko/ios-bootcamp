4cf5d418bb5f04af27776190a40c8e109200832c
Jan Ficko <jan.ficko@gmail.com>
2019-06-21 10:29:28 +0200

    Added bitcoin price api and displayed data

---
+++ BitcoinTicker/BitcoinTicker/Model/BitcoinDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  BitcoinDataModel.swift
+//  BitcoinTicker
+//
+//  Created by Jan Ficko on 21/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
